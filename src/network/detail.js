8e557c8b28771722193a9b1a142f999f0e8bf9ae
KEVEN\liuqi <shide.liu@tx-tech.com>
2021-06-02 18:22:32 +0800

    #详情页开发

--- src/network/detail.js
+++ src/network/detail.js
@@ -23,3 +23,25 @@ export class Goods {
     this.realPrice = itemInfo.lowNowPrice
   }
 }
+
+// 新建一个店铺类
+export class Shop {
+  constructor(shopInfo) {
+    this.logo = shopInfo.shopLogo;
+    this.name = shopInfo.name;
+    this.fans = shopInfo.cFans;
+    this.sells = shopInfo.cSells;
+    this.score = shopInfo.score;
+    this.goodsCount = shopInfo.cGoods
+  }
+}
+
+// 新建一个商品参数类
+export class GoodsParam {
+  constructor(info, rule) {
+    // 注: images可能没有值(某些商品有值, 某些没有值)
+    this.image = info.images ? info.images[0] : '';
+    this.infos = info.set;
+    this.sizes = rule.tables;
+  }
+}
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-images="topImages"></detail-swiper>
-    <detail-base-info :goods="goods"/>
+  <div id="detail">
+    <detail-nav-bar class="detail-nav" />
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"/>
+      <detail-shop-info :shop="shop"/>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
+      <detail-param-info :param-info="paramInfo"/>
+      <!-- 暂时要加这段，不然滚动不了,就是因为再图片加载前计算可滚动区域问题 -->
+      <!-- <ul>
+        <li v-for="n in 17" :key="n">列表{{n}}</li>
+      </ul> -->
+    </scroll>
   </div>
 </template>
 
@@ -10,21 +19,33 @@
 import DetailNavBar from './childComps/DetailNavBar'
 import DetailSwiper from './childComps/DetailSwiper'
 import DetailBaseInfo from './childComps/DetailBaseInfo'
+import DetailShopInfo from './childComps/DetailShopInfo'
+import DetailGoodsInfo from './childComps/DetailGoodsInfo'
+import DetailParamInfo from './childComps/DetailParamInfo'
 
-import { getDetail, Goods } from 'network/detail'
+import Scroll from 'components/common/scroll/Scroll'
+
+import { getDetail, Goods, Shop, GoodsParam } from 'network/detail'
 
 export default {
   name: 'Detail',
   components: {
     DetailNavBar,
     DetailSwiper,
-    DetailBaseInfo
+    DetailBaseInfo,
+    DetailShopInfo,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    Scroll
   },
   data() {
     return {
       iid: null,
       topImages: [],
-      goods: {}
+      goods: {},
+      shop: {},
+      detailInfo: {},
+      paramInfo: {}
     }
   },
   created() {
@@ -38,15 +59,44 @@ export default {
       this.topImages = data.itemInfo.topImages
       // 2.获取商品基本信息
       this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
-
+      // 3.获取店铺信息
+      this.shop = new Shop(data.shopInfo)
+      // 4.获取商品的详情数据
+      this.detailInfo = data.detailInfo
+      // 5.获取参数的信息
+      this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
     })
   },
   methods: {
-
+    imageLoad() { // 表示图片加载完成后，重新计算可滚动区域
+      this.$refs.scroll.refresh()
+    }
   }
 }
 </script>
 
 <style scoped>
-
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+.detail-nav {
+  position: relative;
+  z-index: 10;
+  background-color: #fff;
+}
+/** 通过定位来 */
+/* .content {
+  position: absolute;
+  top: 44px;
+  left: 0;
+  right: 0;
+} */
+/** 通过计算高度 */
+.content {
+  height: calc(100% - 44px);
+  background-color: #fff;
+}
 </style>
